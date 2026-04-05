2bdafc5af5fce45c2f9d39f4cf905d0a534cb05c
Shankar Prasad Pyke <bikramff54@gmail.com>
2026-04-05 19:51:51 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "love-app-v3";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
